be6bd4c973c3955458eed679a794288770142b26
Didier Lafforgue <didier.lafforgue@gmail.com>
2024-01-05 11:26:07 +0100

    chore: don't use hard domain name values in the controller

--- app/controllers/check_domain_controller.rb
+++ app/controllers/check_domain_controller.rb
@@ -1,6 +1,6 @@
 class CheckDomainController < ApplicationController
   def show
-    domain_hosted_here = params[:domain] == 'demo-pro.maglev.dev' || Site.exists?(domain: params[:domain])
+    domain_hosted_here = params[:domain] == Rails.application.config.x.main_host || Site.exists?(domain: params[:domain])
     head domain_hosted_here ? :ok : :not_found
   end
 end
